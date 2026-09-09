3a361f74cfb7485383f11473f7880984daa74e5d
Emmanuel Edidiong Noah <rexxwurld@gmail.com>
2026-09-09 15:13:21 +0100

    Delete src/modules/wallets directory

--- src/modules/wallets/withdrawal.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requireApiKey } = require('../../middleware/auth.middleware');
-const { validateCurrency } = require('../../middleware/currency.middleware');
-const { create, list, getOne } = require('./withdrawal.controller');
-router.post('/', requireApiKey, validateCurrency('currency'), create);
-router.get('/', requireApiKey, list);
-router.get('/:id', requireApiKey, getOne);
-module.exports = router;
